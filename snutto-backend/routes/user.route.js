56c7cc8f7ef1d61b47d02b1090ef60704195e88f
Swaraj-Jadhav <swarajj762@gmail.com>
2025-06-26 22:38:39 +0530

    validate-request added

--- snutto-backend/routes/user.route.js
+++ snutto-backend/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as userController from '../controllers/user.controller.js';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request.js'; // Add this
+import { validateRequest } from './../middlewares/validate-request.js'; 
 
 const router = Router();
 
